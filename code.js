f0d7d6e802eead0ba79afeb4a3d3dc495d9415e7
eunice.kim <eyk5302@nyu.edu>
2022-10-06 01:17:56 -0400

    saving changes

---
+++ code.js
@@ -0,0 +1,225 @@
+var imgOne = document.querySelector('img:nth-child(1)');
+var imgTwo = document.querySelector('img:nth-child(2)');
+var imgThree = document.querySelector('img:nth-child(3)');
+var imgFour = document.querySelector('img:nth-child(4)');
+var imgFive = document.querySelector('img:nth-child(5)');
+var imgSix = document.querySelector('img:nth-child(6)');
+var imgSeven = document.querySelector('img:nth-child(7)');
+var imgEight = document.querySelector('img:nth-child(8)');
+var imgNine = document.querySelector('img:nth-child(9)');
+var imgTen = document.querySelector('img:nth-child(10)');
+var imgEleven = document.querySelector('img:nth-child(11)');
+var imgTwelve = document.querySelector('img:nth-child(12)');
+var imgThirteen = document.querySelector('img:nth-child(13)');
+var imgFourteen = document.querySelector('img:nth-child(14)');
+var imgFifteen = document.querySelector('img:nth-child(15)');
+var imgSixteen = document.querySelector('img:nth-child(16)');
+var imgSeventeen = document.querySelector('img:nth-child(17)');
+var imgEighteen = document.querySelector('img:nth-child(18)');
+var imgNineteen = document.querySelector('img:nth-child(19)');
+var imgTwenty = document.querySelector('img:nth-child(20)');
+var imgTwentyOne = document.querySelector('img:nth-child(21)');
+var imgTwentyTwo = document.querySelector('img:nth-child(22)');
+var imgTwentyThree = document.querySelector('img:nth-child(23)');
+var imgTwentyFour = document.querySelector('img:nth-child(24)');
+var imgTwentyFive = document.querySelector('img:nth-child(25)');
+var imgTwentySix = document.querySelector('img:nth-child(26)');
+var imgTwentySeven = document.querySelector('img:nth-child(27)');
+var imgTwentyEight = document.querySelector('img:nth-child(28)');
+
+function showOne() {
+  imgOne.classList.add("show");
+}
+function hideOne() {
+  imgOne.classList.remove("show");
+}
+
+function showTwo() {
+  imgTwo.classList.add("show");
+}
+function hideTwo() {
+  imgTwo.classList.remove("show");
+}
+
+function showThree() {
+  imgThree.classList.add("show");
+}
+function hideThree() {
+  imgThree.classList.remove("show");
+}
+
+function showFour() {
+  imgFour.classList.add("show");
+}
+function hideFour() {
+  imgFour.classList.remove("show");
+}
+
+function showFive() {
+  imgFive.classList.add("show");
+}
+function hideFive() {
+  imgFive.classList.remove("show");
+}
+
+function showSix() {
+  imgSix.classList.add("show");
+}
+function hideSix() {
+  imgSix.classList.remove("show");
+}
+
+function showSeven() {
+  imgSeven.classList.add("show");
+}
+function hideSeven() {
+  imgSeven.classList.remove("show");
+}
+
+function showEight() {
+  imgEight.classList.add("show");
+}
+function hideEight() {
+  imgEight.classList.remove("show");
+}
+
+function showNine() {
+  imgNine.classList.add("show");
+}
+function hideNine() {
+  imgNine.classList.remove("show");
+}
+
+function showTen() {
+  imgTen.classList.add("show");
+}
+function hideTen() {
+  imgTen.classList.remove("show");
+}
+
+function showEleven() {
+  imgEleven.classList.add("show");
+}
+function hideEleven() {
+  imgEleven.classList.remove("show");
+}
+
+function showTwelve() {
+  imgTwelve.classList.add("show");
+}
+function hideTwelve() {
+  imgTwelve.classList.remove("show");
+}
+
+function showThirteen() {
+  imgThirteen.classList.add("show");
+}
+function hideThirteen() {
+  imgThirteen.classList.remove("show");
+}
+
+function showFourteen() {
+  imgFourteen.classList.add("show");
+}
+function hideFourteen() {
+  imgFourteen.classList.remove("show");
+}
+
+function showFifteen() {
+  imgFifteen.classList.add("show");
+}
+function hideFifteen() {
+  imgFifteen.classList.remove("show");
+}
+
+function showSixteen() {
+  imgSixteen.classList.add("show");
+}
+function hideSixteen() {
+  imgSixteen.classList.remove("show");
+}
+
+function showSeventeen() {
+  imgSeventeen.classList.add("show");
+}
+function hideSeventeen() {
+  imgSeventeen.classList.remove("show");
+}
+
+function showEighteen() {
+  imgEighteen.classList.add("show");
+}
+function hideEighteen() {
+  imgEighteen.classList.remove("show");
+}
+
+function showNineteen() {
+  imgNineteen.classList.add("show");
+}
+function hideNineteen() {
+  imgNineteen.classList.remove("show");
+}
+
+function showTwenty() {
+  imgTwenty.classList.add("show");
+}
+function hideTwenty() {
+  imgTwenty.classList.remove("show");
+}
+
+function showTwentyOne() {
+  imgTwentyOne.classList.add("show");
+}
+function hideTwentyOne() {
+  imgTwentyOne.classList.remove("show");
+}
+
+function showTwentyTwo() {
+  imgTwentyTwo.classList.add("show");
+}
+function hideTwentyTwo() {
+  imgTwentyTwo.classList.remove("show");
+}
+
+function showTwentyThree() {
+  imgTwentyThree.classList.add("show");
+}
+function hideTwentyThree() {
+  imgTwentyThree.classList.remove("show");
+}
+
+
+function showTwentyFour() {
+  imgTwentyFour.classList.add("show");
+}
+function hideTwentyFour() {
+  imgTwentyFour.classList.remove("show");
+}
+
+function showTwentyFive() {
+  imgTwentyFive.classList.add("show");
+}
+function hideTwentyFive() {
+  imgTwentyFive.classList.remove("show");
+}
+
+function showTwentySix() {
+  imgTwentySix.classList.add("show");
+}
+function hideTwentySix() {
+  imgTwentySix.classList.remove("show");
+}
+
+function showTwentySeven() {
+  imgTwentySeven.classList.add("show");
+}
+function hideTwentySeven() {
+  imgTwentySeven.classList.remove("show");
+}
+
+function showTwentyEight() {
+  imgTwentyEight.classList.add("show");
+}
+function hideTwentyEight() {
+  imgTwentyEight.classList.remove("show");
+}
